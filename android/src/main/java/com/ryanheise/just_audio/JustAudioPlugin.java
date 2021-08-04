2a17470f0fa71d45af85610ea4a0699fa8dd9f01
ps6067966 <ps6067966@gmail.com>
2021-08-04 15:27:31 +0530

    pss

---
+++ android/src/main/java/com/ryanheise/just_audio/JustAudioPlugin.java
@@ -0,0 +1,47 @@
+package com.ryanheise.just_audio;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine.EngineLifecycleListener;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/**
+ * JustAudioPlugin
+ */
+public class JustAudioPlugin implements FlutterPlugin {
+    private MethodChannel channel;
+    private MainMethodCallHandler methodCallHandler;
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        Context applicationContext = binding.getApplicationContext();
+        BinaryMessenger messenger = binding.getBinaryMessenger();
+        methodCallHandler = new MainMethodCallHandler(applicationContext, messenger);
+
+        channel = new MethodChannel(messenger, "com.ryanheise.just_audio.methods");
+        channel.setMethodCallHandler(methodCallHandler);
+        binding.getFlutterEngine().addEngineLifecycleListener(new EngineLifecycleListener() {
+            @Override
+            public void onPreEngineRestart() {
+                methodCallHandler.dispose();
+            }
+
+            @Override
+            public void onEngineWillDestroy() {
+            }
+        });
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodCallHandler.dispose();
+        methodCallHandler = null;
+
+        channel.setMethodCallHandler(null);
+    }
+}
